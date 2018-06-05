9ed1f519e03ab00c715a4acb03c091466fd67ad2
Julian Rendon <julian_javier@hotmail.com>
2018-06-04 21:57:34 -0400

    Changed Activity1 comments and removed unused imports

--- app/src/main/java/com/jrendon/a05_onactivityresultexample/Activity1.java
+++ app/src/main/java/com/jrendon/a05_onactivityresultexample/Activity1.java
@@ -2,7 +2,6 @@ package com.jrendon.a05_onactivityresultexample;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,10 +37,9 @@ public class Activity1 extends Activity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // We verify to which intent we're referring here with the help of our Id
-
+        // Check which request we're responding to
         if (requestCode == Spy) {
-            // We verify also which operation was executed
+            // Make sure the request was successful
             if (resultCode == RESULT_OK) {
                 // We display the button that was chosen
                 Toast.makeText(this, "You've chosen Intent " +
